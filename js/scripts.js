a1ceefb66455c728d73b4ed6a961a2b71a577503
craigwann <craigwann@me.com>
2018-06-01 14:40:31 -0700

    add branching to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
   $("#radio").submit(function(event) {
     var company = $("input:radio[name=company]:checked").val();
+    var end = $("input:radio[name=end]:checked").val();
+    var prog = $("input:radio[name=prog]:checked").val();
+    var lego = $("input:radio[name=lego]:checked").val();
+    var micro = $("input:radio[name=micro]:checked").val();
 
-    if (company === "big") {
+
+
+
+    if (company === "big" && end === "back" && micro === "love") {
       $("#csharp").show();
       $("#ruby").hide();
+      $("#react").hide();
+
+    } else if (company === "small" && end === "back" && prog === "webapp" && lego === "shape") {
+      $("#ruby").show();
+      $("#csharp").hide();
+      $("#react").hide();
+
+    } else if (end === "front" && ) {
+      $("#ruby").show();
+      $("#csharp").hide();
+      $("#react").hide();
+
+    } else if (company === "small") {
+      $("#ruby").show();
+      $("#csharp").hide();
+      $("#react").hide();
+
+    } else if (company === "small") {
+      $("#ruby").show();
+      $("#csharp").hide();
       $("#react").hide();
 
     } else if (company === "small") {
